e8fcea34a62b72ed224c330f7e81e46d22b5c903
Eddie Kim <edkm1212@gmail.com>
2026-01-09 15:49:50 -0700

    building basic training game UI card

--- app/wordOfTheDay/app/(tabs)/trainingTab.tsx
+++ app/wordOfTheDay/app/(tabs)/trainingTab.tsx
@@ -1,4 +1,4 @@
-import { View, Pressable, Text } from "react-native";
+import { View, Pressable, Text, ScrollView } from "react-native";
 import { Button } from "react-native-paper";
 import AppHeader from "@/components/appHeader";
 import AppCard from "@/components/appCard";
@@ -11,40 +11,38 @@ export default function TrainingTab() {
     return(
         <View className="flex-1">
             <AppHeader title={LABELS.APP_TITLE} description={LABELS.APP_DESCRIPTION}/>
-            <View className="p-6">
-                <View className="flex-col justify-center w-full h-full">
-                    <View className="bg-violet-500 rounded-2xl shadow-md mb-4">
-                        <View className="flex-col justify-start p-6">
-                            <Text className="text-white text-lg">AI Training Games</Text>
-                            <Text className="text-white text-base">Master vocabulary through adaptive learning</Text>
-                            <Text className="text-white mt-10 text-base">1 words in your study list</Text>
+            <ScrollView>
+                <View className="p-6">
+                    <View className="flex-col justify-center w-full h-full">
+                        <View className="bg-violet-500 rounded-2xl shadow-md mb-4">
+                            <View className="flex-col justify-start p-6">
+                                <Text className="text-white text-lg">AI Training Games</Text>
+                                <Text className="text-white text-base">Master vocabulary through adaptive learning</Text>
+                                <Text className="text-white mt-10 text-base">1 words in your study list</Text>
+                            </View>
+                        </View>
+                        <View className="mb-4">
+                            <TrainingGameCard
+                                title={TRAINING_LABELS.DEFINITION_MATCHING_TITLE}
+                                description="Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi." 
+                                icon=""
+                                onPress={()=>{}}
+                            />
+                        </View>
+                        <View className="mb-4">
+                            <TrainingGameCard
+                                title={TRAINING_LABELS.DEFINITION_MATCHING_TITLE}
+                                description="Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi." 
+                                icon=""
+                                onPress={()=>{}}
+                            />
+                        </View>
+                        <View>
+
                         </View>
                     </View>
-                    <View className="mb-4">
-                        <TrainingGameCard
-                            title={TRAINING_LABELS.DEFINITION_MATCHING_TITLE}
-                            description="" 
-                            icon=""
-                            onPress={()=>{}}
-                        />
-                    </View>
-                    <View className="mb-4">
-                        <AppCard>
-                            <View className="m-6">
-                                <Button
-                                    icon="book-open-variant-outline"
-                                    mode="outlined"
-                                    onPress={()=>console.log("Definition matching")}>
-                                    {TRAINING_LABELS.START_GAME_BUTTON}
-                                </Button>
-                            </View>
-                        </AppCard>
-                    </View>
-                <View>
-
                 </View>
-            </View>
-            </View>
+            </ScrollView>
         </View>
     );
 }
--- app/wordOfTheDay/components/trainingGameCard.tsx
+++ app/wordOfTheDay/components/trainingGameCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import AppCard from "./appCard";
-import { Button } from "react-native-paper";
+import { Button, Chip } from "react-native-paper";
+import { TRAINING_LABELS } from "@/constants/labels";
 
 interface TrainingGameCardProps {
     title: string,
@@ -21,18 +22,32 @@ export default function TrainingGameCard({
             <AppCard>
                 <View className="m-6">
                     <View className="flex-col justify-start">
-                        <Text className="text-lg font-semibold my-4">
+                        <Text className="text-xl font-semibold my-4">
                             {title}
                         </Text>
-                        <Text>
+                        <Text className="my-2">
                             {description}
                         </Text>
-                        <Text>
+                        <Text className="my-4">
                             15-20 mins
                         </Text>
-                        <Text>
+                        <Text className="my-1">
                             Skills Focused: 
                         </Text>
+                        <View className="mt-1 mb-2">
+                            <Chip mode="outlined">
+                                Understanding
+                            </Chip>
+                            <Chip mode="outlined">
+                                Perception
+                            </Chip>
+                        </View>
+                        <Button
+                            icon="book-open-variant-outline"
+                            mode="outlined"
+                            onPress={()=>console.log("Definition matching")}>
+                            {TRAINING_LABELS.START_GAME_BUTTON}
+                        </Button>
                     </View>
                 </View>
             </AppCard>
