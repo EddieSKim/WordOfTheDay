0352e1e506a745c0361d2a658ca45c37fb1233a5
EddieSKim <edkm1212@gmail.com>
2026-03-31 22:48:51 -0600

    adding constants for progress distribution

---
+++ app/wordOfTheDay/constants/distributionCategories.ts
@@ -0,0 +1,7 @@
+export const distributionCategories = {
+    STARTING: "Starting",
+    BEGINNER: "Beginner",
+    INTERMEDIATE: "Intermediate",
+    ADVANCED: "Advanced",
+    MASTERED: "Mastered",
+} as const;
